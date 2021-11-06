ace9c659fbf43ffee72224f9cc8e1f8b714269fc
magicbart <magicbart@gmail.com>
2021-11-06 10:40:32 +0100

    use ServiceEntityRepository

--- Repository/PageRepository.php
+++ Repository/PageRepository.php
@@ -2,10 +2,14 @@
 
 namespace ProjetNormandie\PageBundle\Repository;
 
-use Doctrine\ORM\EntityRepository;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use ProjetNormandie\PageBundle\Entity\Page;
 
-class PageRepository extends EntityRepository
+class PageRepository extends ServiceEntityRepository
 {
-
-
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Page::class);
+    }
 }
