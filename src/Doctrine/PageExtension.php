6a372f080f6c755553de85fcc5b11742169da1d6
magicbart <magicbart@gmail.com>
2022-11-16 12:25:50 +0100

    Fix: Update PageExtension.php

--- src/Doctrine/PageExtension.php
+++ src/Doctrine/PageExtension.php
@@ -1,9 +1,10 @@
 <?php
 namespace ProjetNormandie\PageBundle\Doctrine;
 
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\Intl\Locale;
 use ProjetNormandie\PageBundle\Entity\Page;
@@ -14,14 +15,17 @@ final class PageExtension implements QueryCollectionExtensionInterface, QueryIte
      * @param QueryBuilder                $queryBuilder
      * @param QueryNameGeneratorInterface $queryNameGenerator
      * @param string                      $resourceClass
-     * @param string|null                 $operationName
+     * @param Operation|null              $operation
+     * @param array                       $context
+     * @return void
      */
     public function applyToCollection(
         QueryBuilder $queryBuilder,
         QueryNameGeneratorInterface $queryNameGenerator,
         string $resourceClass,
-        string $operationName = null
-    ) {
+        Operation $operation = null,
+        array $context = []
+    ): void {
         $this->addWhere($queryBuilder, $resourceClass);
     }
 
@@ -30,17 +34,18 @@ final class PageExtension implements QueryCollectionExtensionInterface, QueryIte
      * @param QueryNameGeneratorInterface $queryNameGenerator
      * @param string                      $resourceClass
      * @param array                       $identifiers
-     * @param string|null                 $operationName
+     * @param Operation|null              $operation
      * @param array                       $context
+     * @return void
      */
     public function applyToItem(
         QueryBuilder $queryBuilder,
         QueryNameGeneratorInterface $queryNameGenerator,
         string $resourceClass,
         array $identifiers,
-        string $operationName = null,
+        Operation $operation = null,
         array $context = []
-    ) {
+    ) : void {
         $this->addWhere($queryBuilder, $resourceClass);
     }
 
